df69155eb94436a8d120c671153d13030aa107f7
Sriharsh Sai <sriharsh1997@gmail.com>
2025-01-30 21:14:47 -0500

    types for tables

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from '../types/supabase';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
